5f083df5ac3238929890687f7b7d31681ac2af54
Sabrina82-LS <silvasabluisa25804@gmail.com>
2026-01-28 14:27:30 +0000

    Add API xUnit test project

---
+++ DeliInventoryManagement_1.Api.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace DeliInventoryManagement_1.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Basic_test_should_pass()
+    {
+        Assert.True(true);
+    }
+}
